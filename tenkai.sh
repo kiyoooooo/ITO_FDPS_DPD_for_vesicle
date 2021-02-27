9ba64256f987ede2bea5057458df419a722b76b5
p70293b <p70293b@ito-1.kyushu-u.sc>
2021-02-27 15:09:46 +0900

    展開用のシェル

---
+++ tenkai.sh
@@ -0,0 +1,16 @@
+#!/bin/sh  
+DIR=exp_double4
+
+#tar -xvf vesicle_input3000_withdelete_opti0/archive.tar -C vesicle_input3000_withdelete_opti0/
+#tar -xvf vesicle_input3000_withdelete_opti1/archive.tar -C vesicle_input3000_withdelete_opti1/
+#tar -xvf vesicle_input3000_withdelete_opti2/archive.tar -C vesicle_input3000_withdelete_opti2/
+
+tar -xvf $DIR/archive.tar -C $DIR/
+
+
+#mv vesicle_input3000_withdelete_opti0/output/* vesicle_input3000_withdelete_opti0/
+#mv vesicle_input3000_withdelete_opti1/output/* vesicle_input3000_withdelete_opti1/
+#mv vesicle_input3000_withdelete_opti2/output/* vesicle_input3000_withdelete_opti2/
+
+
+mv $DIR/output/* $DIR/
